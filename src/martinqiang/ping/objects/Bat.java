c33303404143a85977550c1c67c9bf70b56929b8
Martin Qiang <martinqiang@icloud.com>
2021-09-09 17:30:48 +1200

    Created class for bat

--- src/martinqiang/ping/objects/Bat.java
+++ src/martinqiang/ping/objects/Bat.java
@@ -1,4 +1,34 @@
 package martinqiang.ping.objects;
 
-public class Bat {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Bat extends JPanel {
+
+    private BufferedImage batImage;
+    private int yPos = 0;
+
+    public Bat() {
+        try {
+            batImage = ImageIO.read(new File("res/bat.png"));
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        int x = getWidth() - batImage.getWidth();
+        g.drawImage(batImage, x, yPos, this);
+
+    }
 }
